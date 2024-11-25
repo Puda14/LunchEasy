f8a1819e7aa76e79a9641690fcf966a28d7ff862
TuanNH76 <tuannguyenhuy87@gmail.com>
2024-11-25 23:08:46 +0700

    config mongo db in docker compose #1

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ require('dotenv').config();
 
 const app = express();
 
+//mogodb connection
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 // Middleware
 app.use(express.json());
 
