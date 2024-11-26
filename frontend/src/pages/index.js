59ac133a0aaa265265ec8a83e95ae59baec8c24d
CuongNguyen1701 <ntc170103@gmail.com>
2024-11-26 10:08:30 +0000

    implement dynamic export (not tested)

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,2 +1,18 @@
-import Home from "./Home";
-export default Home;
+import { readdirSync, statSync } from 'fs';
+import { join } from 'path';
+
+// Get all folder names in the current directory
+const folderNames = readdirSync(__dirname).filter(file => {
+  return statSync(join(__dirname, file)).isDirectory();
+});
+
+// Dynamically import each folder using es6 (Ex. import Home from './Home')
+const imports = {}
+for (const folderName of folderNames) {
+  import(folderName).then(module => {
+    imports[folderName] = module.default;
+  });
+}
+
+
+export default imports;
