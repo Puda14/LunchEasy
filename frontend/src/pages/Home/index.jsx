7f5312499e8db7a35e4d03993f25004bfa6ce4f1
CuongNguyen1701 <ntc170103@gmail.com>
2024-11-27 06:35:59 +0000

    implement layout wrapper

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,10 @@
 import NavigatorSquare from './components/Navigators.jsx';
 import Sidebar from '../../components/Sidebar.jsx';
+import LayoutWrapper from '../../components/wrappers/LayoutWrapper.jsx';
 const Home = () => {
 
   return (
-      <div className='flex flex-row'>
-        <Sidebar/>
+    <LayoutWrapper element={
         <div className='grid grid-cols-3 gap-12 px-96 py-72'>
           <NavigatorSquare name='おすすめ' route='/recommendation'/>
           <NavigatorSquare name='レストラン' route='#'/>
@@ -12,8 +12,8 @@ const Home = () => {
           <NavigatorSquare name='お気に入り' route='#'/>
           <NavigatorSquare name='設定' route='#'/>
           <NavigatorSquare name='健康的なお勧め' route='#'/>
-        </div>
-      </div>
+        </div>}
+      />
   )
 }
 
