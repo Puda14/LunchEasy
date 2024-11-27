e3d1e9705d836641194d121688f9052c19fcd00d
Mingix-PT <mingixpt@gmail.com>
2024-11-27 13:51:29 +0700

    fix sidebar and home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,18 +1,18 @@
 import NavigatorSquare from './components/Navigators.jsx';
-import Sidebar from '../../components/Sidebar.jsx';
 import LayoutWrapper from '../../components/wrappers/LayoutWrapper.jsx';
 const Home = () => {
 
   return (
-    <LayoutWrapper element={
-        <div className='grid grid-cols-3 gap-12 px-96 py-72'>
+    <LayoutWrapper element={<div className='flex flex-col h-screen w-full justify-center items-center'>
+        <div className='grid grid-cols-3 gap-20 w-2/3 h-2/3'>
           <NavigatorSquare name='おすすめ' route='/recommendation'/>
           <NavigatorSquare name='レストラン' route='#'/>
           <NavigatorSquare name='歴史' route='#'/>
           <NavigatorSquare name='お気に入り' route='#'/>
           <NavigatorSquare name='設定' route='#'/>
           <NavigatorSquare name='健康的なお勧め' route='#'/>
-        </div>}
+        </div>
+    </div>}
       />
   )
 }
