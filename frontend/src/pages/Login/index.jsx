808b067a9219abf153ce94ed76ae0a70acaf3bd8
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-12-03 16:33:21 +0700

    add signup and navigation

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,49 +1,50 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import LandingButton from "../../components/LandingButton";
 import InputField from "../../components/InputField";
 import CheckBox from "../../components/CheckBox";
 import LandingPageWrapper from "../../components/wrappers/LandingPageWrapper";
 const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleRememberChange = (e) => {
-    setRemember(e.target.checked);
-  };
   const handleLogin = () => {
     //TODO: Implement login logic
     alert(`Email: ${email} Password: ${password} Remember: ${remember}`);
     // Navigate to home page
-    window.location.href = "/";
-  };
-  const handleSignup = () => {
-    // <Navigate to="/signup" />
+    navigate("/");
   };
   return (
-    <LandingPageWrapper element={
+    <LandingPageWrapper
+      element={
         <div className="flex flex-col items-center justify-center w-1/2 h-full">
-          <div className="mb-32 text-5xl font-bold">ランチイージー</div>
+          <div className="mb-20 text-5xl font-bold">ランチイージー</div>
           <InputField
             label="ログインメールアドレス"
             type="text"
-            onChange={handleEmailChange}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <InputField
             label="パスワード"
             type="password"
-            onChange={handlePasswordChange}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <CheckBox id="remember-checkbox" text="パスワードを保存する" onChange={handleRememberChange} />
+          <CheckBox
+            id="remember-checkbox"
+            text="パスワードを保存する"
+            onChange={() => setRemember(!remember)}
+          />
 
           <LandingButton text="ログイン" onClick={handleLogin} />
-          <LandingButton text="サインアップ" />
-        </div>}
+          <LandingButton
+            text="サインアップ"
+            onClick={() => {
+              window.location.href = "/signup";
+            }}
+          />
+        </div>
+      }
     />
   );
 };
--- frontend/src/pages/Signup/index.jsx
+++ frontend/src/pages/Signup/index.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import LandingButton from "../../components/LandingButton";
+import InputField from "../../components/InputField";
+import CheckBox from "../../components/CheckBox";
+import BackButton from "../../components/BackButton";
+import LandingPageWrapper from "../../components/wrappers/LandingPageWrapper";
 const Signup = () => {
+  const [birthday, setBirthday] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleSignup = () => {
+    // TODO: Implement signup logic
+    alert(`Birthday: ${birthday} Email: ${email} Password: ${password}`);
+    // Navigate to home page
+    navigate("/");
+  };
   return (
-    <div>
+    <LandingPageWrapper
+      element={
+        <div className="flex flex-col items-center justify-center w-1/2 h-full">
+          <div className="self-start w-10 h-auto mb-14">
+            <BackButton />
+          </div>
+          <InputField
+            label="生年月日"
+            type="date"
+            onChange={(e) => setBirthday(e.target.value)}
+          />
+          <InputField
+            label="ログインメールアドレス"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputField
+            label="パスワード"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* <CheckBox id="remember-checkbox" text="パスワードを保存する" /> */}
+          <LandingButton text="サインアップ" onClick={handleSignup} />
+        </div>
+      }
+    />
+  );
+};
 
-    </div>
-  )
-}
-
-export default Signup
+export default Signup;
