439f461b30d22205b72fe20c4c50413629ac8822
Puda14 <pdathp14@gmail.com>
2024-12-04 13:35:31 +0700

    FE/favorite

--- frontend/src/components/wrappers/LayoutWrapper.jsx
+++ frontend/src/components/wrappers/LayoutWrapper.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Sidebar from "../Sidebar";
+import HomeButton from "../../components/HomeButton";
+
 const LayoutWrapper = ({ element }) => {
   const title = "ランチイージー";
   return (
@@ -8,6 +10,9 @@ const LayoutWrapper = ({ element }) => {
       <div className="flex flex-col items-center w-full h-full px-8 py-2">
         <div className="w-full p-2 text-xl font-extrabold text-center bg-orange-200 rounded-sm">
           {title}
+        </div>
+        <div className="w-full flex justify-start">
+          <HomeButton />
         </div>
         {element}
       </div>
