8b91eb6b5e5a50ff8f56bb8cc2524e6436215629
duoggg <dothuyduong25022003@gmail.com>
2024-12-29 00:49:23 +0700

    fix_logo_food_restaurant

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,16 +6,10 @@ const Sidebar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const defaultIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="35"
-      height="35"
-      fill="currentColor"
-      className="bi bi-house"
-      viewBox="0 0 16 16"
-    >
-      <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
-    </svg>
+    <svg viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g fill="none" fill-rule="evenodd" id="页面-1" stroke="none" stroke-width="1"> <g id="导航图标" transform="translate(-26.000000, -15.000000)"> <g id="编组" transform="translate(26.000000, 15.000000)"> <rect fill="#FFFFFF" fill-opacity="0.01" fill-rule="nonzero" height="24" id="矩形" width="24" x="0" y="0"></rect> <polygon id="路径" points="4.5 9 4.5 21 19.5 21 19.5 9 12 3"></polygon> <polygon id="路径" points="4.5 21 4.5 9 2 11 12 3 22 11 19.5 9 19.5 21" stroke="#212121" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></polygon> <polygon id="路径" points="9.5 14.5 9.5 21 14.5 21 14.5 14.5" stroke="#212121" stroke-linejoin="round" stroke-width="1.5"></polygon> <line id="路径" stroke="#212121" stroke-linecap="round" stroke-width="1.5" x1="4.5" x2="19.5" y1="21" y2="21"></line> </g> </g> </g> </g></svg>
+  );
+  const homeIcon = (
+    <svg viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g fill="none" fill-rule="evenodd" id="页面-1" stroke="none" stroke-width="1"> <g id="导航图标" transform="translate(-26.000000, -15.000000)"> <g id="编组" transform="translate(26.000000, 15.000000)"> <rect fill="#FFFFFF" fill-opacity="0.01" fill-rule="nonzero" height="24" id="矩形" width="24" x="0" y="0"></rect> <polygon id="路径" points="4.5 9 4.5 21 19.5 21 19.5 9 12 3"></polygon> <polygon id="路径" points="4.5 21 4.5 9 2 11 12 3 22 11 19.5 9 19.5 21" stroke="#212121" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></polygon> <polygon id="路径" points="9.5 14.5 9.5 21 14.5 21 14.5 14.5" stroke="#212121" stroke-linejoin="round" stroke-width="1.5"></polygon> <line id="路径" stroke="#212121" stroke-linecap="round" stroke-width="1.5" x1="4.5" x2="19.5" y1="21" y2="21"></line> </g> </g> </g> </g></svg>
   );
   const recommendationIcon = (
     <img
@@ -23,6 +17,9 @@ const Sidebar = () => {
       alt="おすすめ"
       className="w-full h-full"
     />
+  );
+  const foodIcon = (
+   <svg viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M774.8 327.8c-50.6-4.8-97.3 4.3-131 22.7 15.9 20.3 26.1 52.1 26.1 87.9 0 29.2-6.8 55.7-17.9 75.5 28.3 16.9 64.5 28.8 104.6 32.6 96.7 9.2 179.2-32.4 184.2-92.8s-69.3-116.7-166-125.9z" fill="#FFB89A"></path><path d="M67.2 494l1 31c2.2 67.7 26.2 133.6 69.6 190.4 41.6 54.5 99.6 99.2 167.9 129.3 15.2 6.7 32.9-0.2 39.5-15.4 6.7-15.2-0.2-32.9-15.4-39.5-59-26-108.9-64.3-144.4-110.8-29-38-47.5-80.7-54.4-125h762.6c-7 44.8-25.8 87.9-55.4 126.3-36.1 46.8-86.8 85.2-146.8 110.9-15.2 6.5-22.2 24.2-15.7 39.4 4.9 11.4 15.9 18.2 27.6 18.2 4 0 8-0.8 11.8-2.4 144.5-62.2 237-185.3 241.3-321.4l1-31H67.2z" fill="#45484C"></path><path d="M591.9 800.1h-159c-35.2 0-64.1 28.8-64.1 64.1s28.8 64.1 64.1 64.1h159c35.2 0 64.1-28.8 64.1-64.1s-28.9-64.1-64.1-64.1z m0 68.1h-159c-2.1 0-4.1-2-4.1-4.1s2-4.1 4.1-4.1h159c2.1 0 4.1 2 4.1 4.1s-2 4.1-4.1 4.1z" fill="#45484C"></path><path d="M498.1 373.5c-9.6-13.5-28.4-16.6-41.9-6.9-13.5 9.6-16.6 28.4-6.9 41.9 10.8 15.1 16.6 33 16.6 51.7 0 16.6 13.4 30 30 30s30-13.4 30-30c0-31.4-9.6-61.4-27.8-86.7zM432.4 321.8c-17.7-7.1-36.3-10.7-55.5-10.7-82.2 0-149 66.8-149 149 0 16.6 13.4 30 30 30s30-13.4 30-30c0-49.1 39.9-89 89-89 11.5 0 22.6 2.1 33.1 6.4 15.4 6.2 32.8-1.3 39-16.7 6.2-15.4-1.2-32.9-16.6-39z" fill="#33CC99"></path><path d="M549.4 274.7c-46.7-45.6-107.7-70.8-171.8-70.8-64.1 0-125.1 25.1-171.8 70.8-46.1 45.1-74 106-78.6 171.4-1.2 16.5 11.3 30.9 27.8 32 16.5 1.1 30.9-11.3 32-27.8 3.5-50.8 25.1-97.9 60.7-132.7 35.4-34.6 81.5-53.7 129.9-53.7 48.3 0 94.5 19.1 129.9 53.7 35.6 34.8 57.1 81.9 60.7 132.7 1.1 15.8 14.3 27.9 29.9 27.9 0.7 0 1.4 0 2.1-0.1 16.5-1.2 29-15.5 27.8-32-4.6-65.4-32.5-126.3-78.6-171.4zM895.1 385.9c-11.5-19.4-27.7-36.6-48.1-51.2l53.9-58.3c11.2-12.2 10.5-31.2-1.7-42.4s-31.2-10.5-42.4 1.7l-65 70.4c-5-1.8-10.1-3.5-15.3-5l82.4-159.2c7.6-14.7 1.9-32.8-12.9-40.4-14.7-7.6-32.8-1.9-40.4 12.9l-91.2 176.3c-5.5-0.3-11.1-0.5-16.7-0.5-21.9 0-43.5 2.4-64.3 7.2-16.1 3.7-26.2 19.8-22.5 36 3.7 16.1 19.8 26.2 36 22.5 16.3-3.8 33.4-5.7 50.7-5.7 43.6 0 84.2 11.8 114.3 33.3 27.1 19.3 42 44 42 69.5 0 16.6 13.4 30 30 30s30-13.4 30-30c0-23.5-6.3-46.1-18.8-67.1z" fill="#45484C"></path></g></svg>
   );
   const restaurantsIcon = (
     <img
@@ -92,7 +89,7 @@ const Sidebar = () => {
   const pageContent = {
     "/": {
       title: "ホーム",
-      icon: defaultIcon,
+      icon: homeIcon,
     },
     "/login": {
       title: "ログイン",
@@ -152,7 +149,7 @@ const Sidebar = () => {
     },
     "/admin": {
       title: "アドミンホーム",
-      icon: defaultIcon,
+      icon: homeIcon,
     },
     "/admin/restaurant-management": {
       title: "レストランリストの管理",
@@ -169,22 +166,40 @@ const Sidebar = () => {
     // Thêm các đường dẫn khác tại đây
   };
 
-  const currentPage = pageContent[location.pathname] || {
-    title: "未知",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="35"
-        height="35"
-        fill="currentColor"
-        className="bi bi-question-circle"
-        viewBox="0 0 16 16"
-      >
-        <path d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zM8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0z" />
-        <path d="M5.255 5.786a.237.237 0 0 0-.241.247c.015.355.115.716.303 1.005.244.365.595.606 1.03.606.262 0 .538-.11.735-.319.197-.208.331-.475.393-.751.062-.275.054-.558-.025-.816-.08-.26-.243-.503-.464-.657-.221-.154-.495-.236-.776-.236a1.538 1.538 0 0 0-.955.351c-.03.03-.048.066-.071.103zM7.998 11c-.545 0-.998.453-.998 1s.453 1 .998 1 .998-.453.998-1-.453-1-.998-1z" />
-      </svg>
-    ),
-  };
+  let currentPage = pageContent[location.pathname];
+  const currentPath = location.pathname;
+
+  // Kiểm tra nếu đường dẫn là dạng động "/food/{id}"
+  if (!currentPage && (currentPath.startsWith("/food/") ||  currentPath.startsWith("/admin/food/"))) {
+    currentPage = {
+      title: "料理詳細",
+      icon: foodIcon,
+    }
+  }
+
+  if (!currentPage && (currentPath.startsWith("/restaurants/") || currentPath.startsWith("/admin/restaurants/")))  {
+    currentPage = {
+      title: "最寄りのレストラン",
+      icon: restaurantsIcon,
+    }
+  }
+
+  // const currentPage = pageContent[location.pathname] || {
+  //   title: "未知",
+  //   icon: (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       width="35"
+  //       height="35"
+  //       fill="currentColor"
+  //       className="bi bi-question-circle"
+  //       viewBox="0 0 16 16"
+  //     >
+  //       <path d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zM8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0z" />
+  //       <path d="M5.255 5.786a.237.237 0 0 0-.241.247c.015.355.115.716.303 1.005.244.365.595.606 1.03.606.262 0 .538-.11.735-.319.197-.208.331-.475.393-.751.062-.275.054-.558-.025-.816-.08-.26-.243-.503-.464-.657-.221-.154-.495-.236-.776-.236a1.538 1.538 0 0 0-.955.351c-.03.03-.048.066-.071.103zM7.998 11c-.545 0-.998.453-.998 1s.453 1 .998 1 .998-.453.998-1-.453-1-.998-1z" />
+  //     </svg>
+  //   ),
+  // };
   return (
     <div className="top-0 h-full bottom-0 left-0 w-60 min-w-60 max-w-60 d-flex flex-column justify-content-between sidebar">
       <div className="items-center text-center">
